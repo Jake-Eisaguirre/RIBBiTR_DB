f6701eebec968af2748830411bff133ed9ac2342
jake-eisaguirre <eisaguirre@bren.ucsb.edu>
2023-01-27 11:07:40 -0800

    update to add drop columns and sql scripts

--- sql_scripts/audio_build.sql
+++ sql_scripts/audio_build.sql
@@ -50,8 +50,42 @@ references audio_site (site_id);
 alter table audio_info 
 add primary key(audio_id);
 
-select l.location, r.region, r.location, s.site, s.region, v.site
-from audio_location l
-join audio_region r on l.location_id = r.location_id 
-join audio_site s on r.region_id = s.region_id
-join audio_visit v on s.site_id = v.site_id;
+update audio_info a 
+set visit_id = 
+	(select v.visit_id 
+	from audio_visit v 
+	where (v.date_of_deployment, v.site) = (a.date_of_deployment, a.site));
+
+alter table audio_info 
+add constraint fk_audio_info foreign key (visit_id)
+references audio_visit (visit_id);
+
+-- drop columns 
+
+--region
+alter table audio_region 
+drop column region;
+
+-- site
+alter table audio_site 
+drop column region;
+
+-- audio visit
+alter table audio_visit 
+drop column site;
+
+-- info
+alter table audio_info 
+drop column site;
+
+alter table audio_info 
+drop column date_of_deployment;
+
+
+select al.location, ar.location, ar.region, as2.site, as2.region, av.site,
+av.date_of_deployment, ai.site, ai.date_of_deployment
+from audio_location al 
+join audio_region ar on al.location_id = ar.location_id 
+join audio_site as2 on ar.region_id = as2.region_id 
+join audio_visit av on as2.site_id = av.site_id 
+join audio_info ai on av.visit_id = ai.visit_id;
